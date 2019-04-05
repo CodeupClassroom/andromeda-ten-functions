ae1e19013ad2cc74c088df720f44c02cac8aa840
Josh Ramos <josh.r.938@gmail.com>
2019-04-05 16:58:43 -0500

    this is the solutions pre-assesment exercise

--- solutions.js
+++ solutions.js
@@ -20,3 +20,56 @@
  * and
  * concat
  */
+
+
+function isTrue(input) {
+    return input === true;
+}
+
+function isFalse(input) {
+    return input === false;
+}
+
+function not(input) {
+    if (input === false) {
+        return true;
+    } else if (input === true) {
+        return false;
+    } else if (0 >= input) {
+        return true;
+    } else if (input === undefined) {
+        return true;
+    } else if (input > 0) {
+        return false;
+    } else if (typeof input === "string"){
+        return false;
+    } else if (isNaN(input)) {
+        return true;
+    }
+}
+
+function addOne(input) {
+    return +input + 1;
+}
+
+function isEven(input) {
+    return +input % 2 === 0;
+}
+
+function isIdentical(a, b) {
+    return a === b;
+}
+
+function concat(a,b) {
+    return "" + a +b;
+}
+function or(a,b) {
+    return a || b;
+}
+
+function and(a,b) {
+    return a && b;
+}
+function isEqual (a,b) {
+    return a == b;
+}
