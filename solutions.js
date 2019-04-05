c7d551d5f7a0b392776fd1c8873e74b4b495912b
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-05 15:43:39 -0500

    practice exam solutions

--- solutions.js
+++ solutions.js
@@ -20,3 +20,49 @@
  * and
  * concat
  */
+
+function isTrue(input) {
+    return input === true;
+}
+
+
+function isFalse(n) {
+   return n === false;
+}
+
+function not(input2) {
+    return !input2;
+}
+
+function addOne(x) {
+    return parseFloat(x) + 1;
+}
+
+function isEven(number) {
+    if (number % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isIdentical(y, z) {
+    return y === z;
+}
+
+function isEqual(a, b) {
+    return a == b;
+}
+
+function or (c, d) {
+    return c || d;
+}
+
+function and (e, f) {
+    return e && f;
+}
+
+function concat (g, h) {
+    return "" + g + h;
+
+}
