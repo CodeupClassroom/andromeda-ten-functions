af2548f8e4209b6787ef22233c3d0a30d0e8bb35
Edwin Buendia <edwinbuendia@Edwins-MacBook-Pro.local>
2019-04-05 16:55:38 -0500

    practice assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,60 @@
  * and
  * concat
  */
+
+function isTrue(input) {
+  if (input === true) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+function isFalse(input) {
+    if (input === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// boolean switch (!input)
+function not(input) {
+    return !input;
+}
+// look up parseFloat (LEARN IT)
+function addOne(input) {
+    return parseFloat(input) + 1;
+}
+// input % 2 === 0 (even numbers)
+function isEven(input) {
+    if (input ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// simplified if/else function
+function isIdentical(input1,input2) {
+    return input1 === input2;
+}
+
+function isEqual(input1,input2) {
+    if (input1 == input2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function or(input1,input2) {
+    return input1 || input2;
+}
+
+function and(input1,input2) {
+    return input1 && input2;
+}
+
+function concat(input1,input2) {
+    // return String(input1) + String(input2);
+    return input1.toString() + input2.toString();
+}
